ebdb9d709225af15894d6dcf84600726fd5dd9a5
Lisi Gonzalez <lisigonzalez@outlook.com>
2024-04-23 18:17:04 -0300

    add loader, some routes  and changes in buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Heart from './pages/Heart';
 import './styles.css';
 import CustomButton from './components/Button';
 import { NavBar } from './components/NavBar';
-
-
+import AboutUs from './pages/AboutUs'; 
+import Loader from './components/Loader';
 
 
 
 function App() {
-    
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula una carga inicial
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Tiempo de carga simulado: 2 segundos
+  }, []);
+  
   return (
-    <div>
-      <NavBar />
-      <h1 className="pt-16">Alta Cita</h1>
-      <Heart  />
-      <CustomButton defaultColor="#9B59B6" text="hola"/>
-      
-      <CustomButton defaultColor="red" text="te amo franquito" /> 
-      
-     
-    </div>
+    <Router>
+       {loading && <Loader />} 
+      <div className="pt-16">
+        <NavBar />
+        <Routes>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/" element={<Heart />} />
+          
+        </Routes>
+        <CustomButton defaultColor="#9B59B6" text="holaa" />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const CustomButton = ({ text, defaultColor }) => {
   const handleHover = (e) => {
-    e.target.style.boxShadow = `0 0 10px ${defaultColor}, 0 0 40px ${defaultColor}, 0 0 80px ${defaultColor}`;
+    e.target.style.boxShadow = `0 0 10px ${defaultColor}, 0 0 20px ${defaultColor}, 0 0 40px ${defaultColor}`;
   };
 
   const handleMouseOut = (e) => {
-    e.target.style.boxShadow = "none";
+    e.target.style.boxShadow = `0 0 5px ${defaultColor}, 0 0 10px ${defaultColor}, 0 0 15px ${defaultColor}`;
   };
 
   const handleClick = (e) => {
@@ -16,8 +16,14 @@ const CustomButton = ({ text, defaultColor }) => {
   return (
     <button
       type="button"
-      className="text-white border focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-      style={{ backgroundColor: defaultColor, borderColor: defaultColor }}
+      className="text-white border focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2
+                 md:text-base md:px-8 md:py-3 md:me-4 md:mb-4 lg:text-lg lg:px-10 lg:py-3 lg:me-4 lg:mb-4"
+      style={{ 
+        backgroundColor: defaultColor, 
+        borderColor: defaultColor, 
+        boxShadow: `0 0 5px ${defaultColor}, 0 0 10px ${defaultColor}, 0 0 15px ${defaultColor}`,
+        transition: "box-shadow 0.3s ease",
+      }}
       onMouseOver={handleHover}
       onMouseOut={handleMouseOut}
       onClick={handleClick}
